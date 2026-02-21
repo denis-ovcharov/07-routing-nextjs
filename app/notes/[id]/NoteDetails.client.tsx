20548a96977727c08a703b760fc8f2a73e35249c
Denys Ovcharov <denisovcharov88@gmail.com>
2026-02-21 19:21:27 +0100

    'update'

--- app/notes/[id]/NoteDetails.client.tsx
+++ app/notes/[id]/NoteDetails.client.tsx
@@ -22,6 +22,16 @@ const NoteDetailsClient = () => {
 
   if (error || !note) return <p>Some error..</p>;
 
+  const formatDate = (iso: string) => {
+    const date = new Date(iso);
+    return date.toLocaleString("uk-UA", {
+      year: "numeric",
+      month: "2-digit",
+      day: "2-digit",
+      hour: "2-digit",
+      minute: "2-digit",
+    });
+  };
   return (
     <div className={css.container}>
       <div className={css.item}>
@@ -29,8 +39,15 @@ const NoteDetailsClient = () => {
           <h2>{note.title}</h2>
         </div>
         <p className={css.tag}>{note.tag}</p>
-        <p className={css.content}>{note.content}</p>
-        <p className={css.date}>{note.createdAt}</p>
+        <p>{note.content}</p>
+        <p>
+          <span className={css.text}>Created at: </span>
+          {formatDate(note.createdAt)}
+        </p>
+        <p>
+          <span className={css.text}>Last updated at: </span>
+          {formatDate(note.updatedAt)}
+        </p>
       </div>
     </div>
   );
